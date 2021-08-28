314967a402c0c6af3e8057ded81404674a568b26
Su-bro <tngud941208@naver.com>
2021-08-28 22:56:23 +0900

    week1:프로그래머스_신규 아이디 추천

---
+++ 하반기/src/week1/Solution_신규아이디추천.java
@@ -0,0 +1,21 @@
+package week1;
+
+public class Solution_신규아이디추천 {
+	
+	public static String solution(String new_id) {
+        String answer = "";
+        char[] chars = new_id.toCharArray();
+        System.out.println(chars[1]);
+        return answer;
+    }
+	public static void main(String[] args) {
+		String aa = solution("ssss");
+		aa.toUpperCase();
+		String bb = "bB";
+		bb = bb.toLowerCase();
+		System.out.println(bb);
+	}
+	
+	
+
+}

98a79aa3ca7c3c866ae2933927db2ba09a7c4dba
Su-bro <tngud941208@naver.com>
2022-12-14 18:48:41 +0900

    BOJ1620

--- 2023/alg/src/BOJ1620/Main.java
+++ 2023/alg/src/BOJ1620/Main.java
@@ -1,11 +1,45 @@
 package BOJ1620;
 
-import java.io.FileInputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
 
 public class Main {
 
+    static int N, M;
+    static HashMap<Integer, String> map = new HashMap<>();
+    static HashMap<String, Integer> map4s = new HashMap<>();
+
     public static void main(String[] args) throws Exception {
-        FileInputStream fis = new FileInputStream();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        for (int i = 1; i <= N; i++) {
+            String str = br.readLine();
+            map.put(i, str);
+            map4s.put(str, i);
+        }
+
+        for (int i = 0; i < M; i++) {
+            String temp = br.readLine();
+            if (isNumeric(temp)) {
+                System.out.println(map.get(Integer.parseInt(temp)));
+            } else {
+                System.out.println(map4s.get(temp));
+            }
+        }
+
+    }
+    public static boolean isNumeric(String s) {
+        try {
+            Double.parseDouble(s);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 
 }

95b8b1428a98f180b79bf410c27c52aea034ce6d
Su-bro <tngud941208@naver.com>
2024-08-11 15:36:51 +0900

    BOJ1927

---
+++ 2024/src/BOJ1927/Main.java
@@ -0,0 +1,32 @@
+package BOJ1927;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        HashMap<String, Integer> map = new HashMap<>();
+        map.keySet().iterator().next()
+//        System.setIn(new FileInputStream("src/BOJ1927/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        Queue<Integer> pq = new PriorityQueue<>();
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int x = Integer.parseInt(br.readLine());
+            if (x == 0) {
+                if (pq.isEmpty()) {
+                    bw.write("0\n");
+                } else {
+                    bw.write(pq.poll() + "\n");
+                }
+            } else {
+                pq.add(x);
+            }
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
